d1f3c672f9a4ccee0b8c26b888961e0997016418
pierregianferrara <pgianfer@andrew.cmu.edu>
2021-01-04 23:53:35 -0500

    second commit

---
+++ All_code/Humans/ToRunHumans.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SCORES is the file with all the subject names & conditions
+SCORES="Summary_Scores_ALL.txt"
+#OUTPUT is the desired output prefix for the output files
+OUTPUT="Humans_Final"
+
+mkdir Game_Cycles
+mkdir Discrete_Game_Cycles
+mkdir Outputs
+mkdir Preproc
+cd Game_Cycles
+mkdir NoRotations
+mkdir Rotations
+cd ../Discrete_Game_Cycles
+mkdir NoRotations
+mkdir Rotations
+cd ../Scripts
+echo "Preprocessing event files 1/6"
+python Preprocess.py
+echo "Creating Game Cycles 2/6"
+python GameCycles.py
+echo "Creating Discrete game cycles 3/6"
+python DTS.py
+echo "Computing Entropy 4/6"
+python Entropy.py
+echo "Computing Autocorrelation measures 5/6"
+python ACF.py
+echo "Computing Resets, Deflations & Misses 6/6"
+python ResDefMis.py
+#echo "DONE"
